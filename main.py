93a4ebf25cbf9adde2241b3580bd5497d02b80ec
Gilbert <lucas@gilbert.codes>
2023-10-09 21:56:11 +0800

    created static relation and query

--- main.py
+++ main.py
@@ -2,12 +2,55 @@ from graphene import Schema, ObjectType, String , Int, Field, List, Mutation
 from fastapi import FastAPI
 from starlette_graphene3 import GraphQLApp, make_playground_handler
 
-class Query(ObjectType):
-    hello = String(name=String(default_value="graphql"))
+employers_data = [
+    {"id": 1, "name": "MetaTechA", "contact_email": "contact@company-a.com", "industry": "Tech"},
+    {"id": 2, "name": "MoneySoftB", "contact_email": "contact@company-b.com", "industry": "Finance"},
+]
+
+jobs_data = [
+    {"id": 1, "title": "Software Engineer", "description": "Develop web applications", "employer_id": 1},
+    {"id": 2, "title": "Data Analyst", "description": "Analyze data and create reports", "employer_id": 1},
+    {"id": 3, "title": "Accountant", "description": "Manage financial records", "employer_id": 2},
+    {"id": 4, "title": "Manager", "description": "Manage people who manage records", "employer_id": 2},
+]
+
+# 💁 lambda the actual evaluation of the type is postponed until it is needed, allowing both 
+# classes to be defined without running in to issues related to circular dependencies
+
+class EmployerObject(ObjectType):
+    id = Int()
+    name = String()
+    contact_email = String()
+    industry = String()
+    jobs = List(lambda: JobObject) # "lambda" defer undefined class names
     
     @staticmethod
-    def resolve_hello(root, info, name):
-        return f"hello {name}" 
+    def resolve_jobs(root, infor):
+        return [job for job in jobs_data if job["employer_id"] == root["id"]]
+class JobObject(ObjectType):
+    id = Int()
+    title = String()
+    description = String()
+    employer_id = Int()
+    employer = Field(lambda:EmployerObject) # a job is associated with a single employer
+    # The next() function is used to retrieve the next item from
+    # the iterable created by the generator expression. In this case, it will
+    # return the first employer that matches the condition.
+    @staticmethod
+    def resolve_employer(root, info):
+        return next((employer for employer in employers_data if employer["id"] == root["employer_id"]), None)
+
+class Query(ObjectType):
+    jobs = List(JobObject)
+    employers = List(EmployerObject)
+    
+    @staticmethod
+    def resolve_jobs(root, info):
+        return jobs_data
+    
+    @staticmethod
+    def resolve_employers(root, info):
+        return employers_data
     
 schema = Schema(query=Query)
 
