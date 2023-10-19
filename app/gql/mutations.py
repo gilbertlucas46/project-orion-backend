1528f5e74a73475fb7f136b67060f9891fe0bb2b
Gilbert <lucas@gilbert.codes>
2023-10-20 07:25:22 +0800

    create update mutations

--- app/gql/mutations.py
+++ app/gql/mutations.py
@@ -2,6 +2,7 @@ from graphene import Mutation, String, Int, Field, ObjectType
 from app.gql.types import JobObject
 from app.db.database import Session
 from app.db.models import Job
+from sqlalchemy.orm import joinedload
 
 class AddJob(Mutation):
      class Arguments:
@@ -17,9 +18,46 @@ class AddJob(Mutation):
          session = Session()
          session.add(job)
          session.commit()
-         session.refresh() #we're refreshing the job instance with the current state that it has in the db
+         session.refresh(job) #we're refreshing the job instance with the current state that it has in the db
          return AddJob(job=job)
-     
-     
+
+class UpdateJob(Mutation):
+	class Arguments:
+		job_id = Int(required=True)
+		title = String()
+		description = String()
+		employer_id = Int()
+
+	job = Field(lambda: JobObject)
+
+	@staticmethod
+	def mutate(root, info, job_id, title=None, description=None, employer_id=None):
+		session = Session()
+		# job = session.query(Job).filter(Job.id == job_id).first()
+		job = session.query(Job)\
+			.options(joinedload(Job.employer))\
+			.filter(Job.id == job_id).first() 
+   # joinedload() it specifies SQLAlchemy that we want to load the job record alongside with its
+   # relational attribute "employer"
+   # meaning do not lazily evaluate this attribute "employer" do not wait
+   # for somebody to sa .employer, do it right away!
+
+		if not job:
+			raise Exception("Job not found")
+
+		if title is not None:
+			job.title = title
+		if description is not None:
+			job.description = description
+		if employer_id is not None:
+			job.employer_id = employer_id
+
+		session.add(job)
+		session.commit()
+		session.refresh(job) #we're refreshing the job instance with the current state that it has in the db
+		return AddJob(job=job)
+
+
 class Mutation(ObjectType):
     add_job = AddJob.Field()
+    update_job = UpdateJob.Field()
