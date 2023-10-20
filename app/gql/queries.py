2a75cccbc960ee30411638b555e362fa0f4d467e
Gilbert <lucas@gilbert.codes>
2023-10-20 11:28:17 +0800

    add query by ID

--- app/gql/queries.py
+++ app/gql/queries.py
@@ -1,16 +1,21 @@
-from graphene import ObjectType, List
+from graphene import ObjectType, List, Field, Int
 from app.gql.types import JobObject, EmployerObject
 from app.db.database import Session
 from app.db.models import Job, Employer
 
 class Query(ObjectType):
     jobs = List(JobObject)
+    job = Field(JobObject, id=Int(required=True))
     employers = List(EmployerObject)
-    
+
+    @staticmethod
+    def resolve_job(root, info, id):
+        return Session().query(Job).filter(Job.id == id).first()
+
     @staticmethod
     def resolve_jobs(root, info):
         return Session().query(Job).all()
-    
+
     @staticmethod
     def resolve_employers(root, info):
         return Session().query(Employer).all()
