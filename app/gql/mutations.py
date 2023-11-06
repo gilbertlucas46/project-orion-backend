7fa33c2fb38f0dcae1b48dd09a88163a1fb3aa12
Gilbert <lucas@gilbert.codes>
2023-11-06 22:51:28 +0800

    created add ser mutation

--- app/gql/mutations.py
+++ app/gql/mutations.py
@@ -1,7 +1,7 @@
 from graphene import ObjectType
 from app.gql.job.mutations import AddJob, UpdateJob, DeleteJob
 from app.gql.employer.mutations import AddEmployer, UpdateEmployer, DeleteEmployer
-from app.gql.user.mutations import LoginUser
+from app.gql.user.mutations import LoginUser, AddUser
 class Mutation(ObjectType):
     add_job = AddJob.Field()
     update_job = UpdateJob.Field()
@@ -10,3 +10,4 @@ class Mutation(ObjectType):
     update_employer = UpdateEmployer.Field()
     delete_employer = DeleteEmployer.Field()
     login_user = LoginUser.Field()
+    add_user = AddUser.Field()
--- app/gql/user/mutations.py
+++ app/gql/user/mutations.py
@@ -4,7 +4,8 @@ from app.db.database import Session
 from app.db.models import User
 from argon2.exceptions import VerifyMismatchError
 from app.utils.utils import generate_token, verify_password
-
+from app.gql.types import UserObject
+from app.utils.utils import hash_password
 
 class LoginUser(Mutation):
     class Arguments:
@@ -26,3 +27,27 @@ class LoginUser(Mutation):
         token = generate_token(email)
         
         return LoginUser(token=token)
+    
+class AddUser(Mutation):
+    class Arguments:
+        username = String(required=True)
+        email = String(required=True)
+        password = String(required=True)
+        role = String(required=True)
+        
+    user = Field(lambda: UserObject)
+    
+    @staticmethod
+    def mutate(root, info, username, email, password, role):
+        session = Session()
+        user = session.query(User).filter(User.email == email).first()
+        
+        if(user):
+            raise GraphQLError("A user with this email already exists.")
+        
+        password_hash = hash_password(password)
+        user = User(username=username, email=email,password_hash=password_hash, role=role)
+        session.add(user)
+        session.commit()
+        session.refresh(user)
+        return AddUser(user=user)
