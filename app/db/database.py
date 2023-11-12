3b571e731b017e3f9d8d72622cc59607c78e70d6
Gilbert <lucas@gilbert.codes>
2023-11-12 15:23:52 +0800

    removed unused files

--- app/db/database.py
+++ app/db/database.py
@@ -10,7 +10,7 @@ import os
 load_dotenv()
 DB_URL = os.getenv("DB_URL")
 
-engine = create_engine(DB_URL, echo=False)
+engine = create_engine(DB_URL, echo=True)
 Session = sessionmaker(bind=engine)
 
 # database helper
