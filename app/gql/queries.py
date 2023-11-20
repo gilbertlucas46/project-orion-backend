5bc947b43f04f049745decb6dd2e083611e0803f
Gilbert <lucas@gilbert.codes>
2023-11-20 21:59:19 +0800

    fixed user fetch

--- app/gql/queries.py
+++ app/gql/queries.py
@@ -2,7 +2,7 @@ from graphene import ObjectType, List, Field, Int
 from app.gql.types import JobObject, EmployerObject, UserObject, JobApplicationObject, PostObject
 from app.db.database import Session
 from app.db.models import Job, Employer, User, JobApplication
-from app.utils.utils import admin_user
+from app.utils.utils import admin_user, get_authenticated_user
 
 
 class Query(ObjectType):
@@ -13,6 +13,7 @@ class Query(ObjectType):
     users = List(UserObject)
     job_applications = List(JobApplicationObject)
     post = Field(PostObject, id=Int(required=True))
+    me = Field(UserObject)
 
     def resolve_job_applications(root, info):
         return Session().query(JobApplication).all()
@@ -36,3 +37,7 @@ class Query(ObjectType):
     @staticmethod
     def resolve_employers(root, info):
         return Session().query(Employer).all()
+
+    def resolve_me(root, info):
+        user = get_authenticated_user(info.context)
+        return user
